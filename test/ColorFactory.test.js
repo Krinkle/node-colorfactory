cd79287c09eb0e7118eb4a45811786c65d0640b7
Timo Tijhof <krinkle@fastmail.com>
2024-06-30 21:32:20 +0100

    build: 2024 updates, Testem, ESLint 8, adopt `QUnit.test.each()`
    Remove QUnit.test() wrappers and other undocumented customisations
    in favour of QUnit assertion plugins and QUnit.test.each().
    
    Avoiding Ubuntu 24 right now because the ubuntu24 GitHub Actions image
    is missing the firefox binary.
    
    Ref https://github.com/actions/runner-images/issues/9848.

--- test/ColorFactory.test.js
+++ test/ColorFactory.test.js
@@ -1,24 +1,31 @@
 /* eslint-env qunit */
+/* global eachKeyed, eachFmt */
+
 QUnit.module('ColorFactory');
 
-QUnit.colorTest.closeMatch(
-	'primaryColors()',
-	ColorFactory.primaryColors(),
-	['yellow', 'red', 'blue']
-);
+QUnit.test('primaryColors()', function (assert) {
+	assert.colorArrayCloseTo(ColorFactory.primaryColors(), ['yellow', 'red', 'blue']);
+});
 
-QUnit.colorTest.closeMatch('primaryAndSecondaryColors()',
-	ColorFactory.primaryAndSecondaryColors(),
-	['yellow', 'Orange', 'red', '#90f', 'blue', '#0f6']
-);
+QUnit.test('primaryAndSecondaryColors()', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.primaryAndSecondaryColors(),
+		['yellow', 'Orange', 'red', '#90f', 'blue', '#0f6']
+	);
+});
 
-QUnit.colorTest.complementary('#ff0000', '#00ffff');
+QUnit.test.each('complementaryColors()', eachKeyed(0, [
+	['#ff0000', '#00ffff'],
+	['#0338ff', '#ff9d04'],
+	['#9e01d6', '#3ad501']
+]), function (assert, [input, expected]) {
+	assert.colorArrayCloseTo(
+		ColorFactory.complementary(input),
+		[expected, input]
+	);
+});
 
-QUnit.colorTest.complementary('#0338ff', '#ff9d04');
-
-QUnit.colorTest.complementary('#9e01d6', '#3ad501');
-
-[
+QUnit.test.each('analogous', eachKeyed(1, [
 	['#6d00ff', '#0000ff', '#0090ff'],
 	['#ff7400', '#ff0000', '#cd0074'],
 	['#9fee00', '#ffff00', '#ffd300'],
@@ -26,115 +33,142 @@ QUnit.colorTest.complementary('#9e01d6', '#3ad501');
 	['#FE0435', '#FC04C5', '#8B16FB'],
 	['#FF7590', '#FD74DF', '#BF7EFD'],
 	['#0a8ccd', '#04FBD1', '#04fb56']
-].forEach(function (item) {
-	QUnit.colorTest.closeMatch('analogous (from: ' + item[1] + ')',
+]), function (assert, item) {
+	assert.colorArrayCloseTo(
 		ColorFactory.analogous(item[1]),
 		item
 	);
 });
 
-QUnit.colorTest.closeMatch('analogous (from: #04FBD1, degree: 10°)',
-	ColorFactory.analogous('#04FBD1', 10),
-	['#0acdcd', '#04FBD1', '#04FB91']
-);
+QUnit.test.each('analogous', eachFmt('from $0 degree $1°', [
+	['#04FBD1', 10, ['#0acdcd', '#04FBD1', '#04FB91']],
+	['#04FBD1', 80, ['#2a0acd', '#04FBD1', '#80fb04']]
+]), function (assert, [from, degree, expected]) {
+	assert.colorArrayCloseTo(
+		ColorFactory.analogous(from, degree),
+		expected
+	);
+});
 
-QUnit.colorTest.closeMatch('analogous (from: #04FBD1, degree: 80°)',
-	ColorFactory.analogous('#04FBD1', 80),
-	['#2a0acd', '#04FBD1', '#80fb04']
-);
+QUnit.test('interpolate() does nothing if passed one color', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('#e6e6e6', '#e6e6e6', 2),
+		['#e6e6e6', '#e6e6e6']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() does nothing if passed one color',
-	ColorFactory.interpolate('#e6e6e6', '#e6e6e6', 2),
-	['#e6e6e6', '#e6e6e6']
-);
+QUnit.test('interpolate() does nothing if passed two colors', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('#e6e6e6', '#bbbbbb', 2),
+		['#e6e6e6', '#bbbbbb']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() does nothing if passed two colors',
-	ColorFactory.interpolate('#e6e6e6', '#bbbbbb', 2),
-	['#e6e6e6', '#bbbbbb']
-);
+QUnit.test('interpolate() finds medium gray', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('#000000', '#ffffff', 3),
+		['#000000', '#999999', '#ffffff']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() finds medium gray',
-	ColorFactory.interpolate('#000000', '#ffffff', 3),
-	['#000000', '#999999', '#ffffff']
-);
+QUnit.test('interpolate() finds many grays', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('#000000', '#ffffff', 6),
+		['#000000', '#333333', '#666666', '#999999', '#cccccc', '#ffffff']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() finds many grays',
-	ColorFactory.interpolate('#000000', '#ffffff', 6),
-	['#000000', '#333333', '#666666', '#999999', '#cccccc', '#ffffff']
-);
+QUnit.test('interpolate() interpolates hues', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('red', 'blue', 3),
+		['red', '#00ff00', 'blue']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() interpolates hues',
-	ColorFactory.interpolate('red', 'blue', 3),
-	['red', '#00ff00', 'blue']
-);
+QUnit.test('interpolate() interpolates saturation', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('#ff0000', '#996666', 3),
+		['#ff0000', '#cc3333', '#996666']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() interpolates saturation',
-	ColorFactory.interpolate('#ff0000', '#996666', 3),
-	['#ff0000', '#cc3333', '#996666']
-);
+QUnit.test('interpolate() interpolates lightness', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.interpolate('#ff0000', '#ffffff', 5),
+		['#ff0000', '#e75858', '#df9f9f', '#e7d7d7', '#ffffff']
+	);
+});
 
-QUnit.colorTest.closeMatch('interpolate() interpolates lightness',
-	ColorFactory.interpolate('#ff0000', '#ffffff', 5),
-	['#ff0000', '#e75858', '#df9f9f', '#e7d7d7', '#ffffff']
-);
+QUnit.test('qualitative() reproduces example', function (assert) {
+	assert.colorIsDistinguishable(ColorFactory.qualitative('#80b696', 3));
+});
 
-QUnit.colorTest.distinguishable('qualitative() reproduces example',
-	ColorFactory.qualitative('#80b696', 3)
-);
+QUnit.test('qualitative() returns 5 saturated colors when asked', function (assert) {
+	assert.colorIsDistinguishable(ColorFactory.qualitative('red', 5));
+});
 
-QUnit.colorTest.distinguishable('qualitative() returns 5 saturated colors when asked',
-	ColorFactory.qualitative('red', 5)
-);
+QUnit.test('qualitative() is able to start from white', function (assert) {
+	assert.colorIsDistinguishable(ColorFactory.qualitative('white', 5));
+});
 
-QUnit.colorTest.distinguishable('qualitative() is able to start from white',
-	ColorFactory.qualitative('white', 5)
-);
+QUnit.test('qualitative() is able to start from black', function (assert) {
+	assert.colorIsDistinguishable(ColorFactory.qualitative('black', 4));
+});
 
-QUnit.colorTest.distinguishable('qualitative() is able to start from black',
-	ColorFactory.qualitative('black', 4)
-);
+QUnit.test('qualitative() is to start with no color', function (assert) {
+	assert.colorIsDistinguishable(ColorFactory.qualitative(6));
+});
 
-QUnit.colorTest.distinguishable('qualitative() is to start with no color',
-	ColorFactory.qualitative(6)
-);
+QUnit.test('qualitative() returns 9 colors when asked', function (assert) {
+	var colors = ColorFactory.qualitative(ColorHelper.lighten(ColorHelper.saturate('red', -20), 20), 9);
+	assert.strictEqual(colors.length, 9, 'validate array size');
+	assert.colorIsDistinguishable(colors);
+});
 
-QUnit.colorTest.distinguishable('qualitative() returns 9 colors when asked',
-	ColorFactory.qualitative(ColorHelper.lighten(ColorHelper.saturate('red', -20), 20), 9),
-	function (assert) {
-		assert.equal(this.colors.length, 9, 'validate array size');
-	}
-);
+QUnit.test('sequential() reproduces example gray', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.sequential('#e6e6e6', 4),
+		['#e6e6e6', '#b3b3b3', '#5d5d5d', '#202020']
+	);
+});
 
-QUnit.colorTest.closeMatch('sequential() reproduces example gray',
-	ColorFactory.sequential('#e6e6e6', 4),
-	['#e6e6e6', '#b3b3b3', '#5d5d5d', '#202020']
-);
-
-
-QUnit.colorTest.closeMatch('sequential() reproduces example purple',
+QUnit.test('sequential() reproduces example purple', function (assert) {
+	assert.colorArrayCloseTo(
 		ColorFactory.sequential('#c4b3d8', '#240d5e', 3),
 		['#c4b3d8', '#7c67ab', '#240d5e'],
 		25 // custom threshold
-);
+	);
+});
 
-QUnit.colorTest.closeMatch('sequential() works from dark purple lighter',
-	ColorFactory.sequential('#240d5e', 7),
-	['#240d5e', '#34138a', '#4519b5', '#561fe0', '#764ae6', '#9775ec', '#b8a1f2']
-);
+QUnit.test('sequential() works from dark purple lighter', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.sequential('#240d5e', 7),
+		['#240d5e', '#34138a', '#4519b5', '#561fe0', '#764ae6', '#9775ec', '#b8a1f2']
+	);
+});
 
-QUnit.colorTest.closeMatch('sequential() works from light purple darker',
-	ColorFactory.sequential('#c4b3d8', 7),
-	['#c3b2d7', '#ac93c8', '#9475b8', '#7d57a8', '#66478a', '#50376c', '#39284d']
-);
+QUnit.test('sequential() works from light purple darker', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.sequential('#c4b3d8', 7),
+		['#c3b2d7', '#ac93c8', '#9475b8', '#7d57a8', '#66478a', '#50376c', '#39284d']
+	);
+});
 
-QUnit.colorTest.closeMatch('sequential() reproduces example orange',
-	ColorFactory.sequential('#ffcc80', '#b30000', 3),
-	['#ffcc80', '#f35926', '#b30000']
-);
+QUnit.test('sequential() reproduces example orange', function (assert) {
+	assert.colorArrayCloseTo(
+		ColorFactory.sequential('#ffcc80', '#b30000', 3),
+		['#ffcc80', '#f35926', '#b30000']
+	);
+});
 
-
-QUnit.colorTest.binary('binary() reproduces example gray', '#a6a6a6', '#e6e6e6');
-
-QUnit.colorTest.binary('binary() reproduces example green', '#80b696', '#cce8d7');
-
-QUnit.colorTest.binary('binary() reproduces example pink', '#de68a6', '#fbb4d9', 15);
+QUnit.test.each('binary()', {
+	'reproduces example gray': ['#a6a6a6', '#e6e6e6'],
+	'reproduces example green': ['#80b696', '#cce8d7'],
+	'reproduces example pink': ['#de68a6', '#fbb4d9', 15]
+}, function (assert, [input, expected, threshold]) {
+	assert.colorArrayCloseTo(
+		ColorFactory.binary(input),
+		[input, expected],
+		threshold
+	);
+});
--- test/testinit.js
+++ test/testinit.js
@@ -1,91 +1,47 @@
-/* eslint-env browser, es6, qunit */
-
-var domReady = new Promise(function (resolve) {
-	if (/complete/.test(document.readyState)) resolve();
-	else document.addEventListener('readystatechange', function () {
-		if (/complete/.test(document.readyState)) resolve();
-	});
-});
+/* eslint-env browser, qunit */
+/* exported eachKeyed, eachFmt */
 
 function element(tagName, props) {
 	return Object.assign(document.createElement(tagName), props || {});
 }
 
-function renderColors(colors, target, label) {
-	domReady.then(function () {
-		var i, holder,
-			swatches = element('div', { className: 'test-colorFactory-swatches' });
-
-		for (i = 0; i < colors.length; i++) {
-			swatches.append(
-				element('div', {
-					className: 'test-colorFactory-swatch',
-					style: 'background-color:' + colors[colors.length - i - 1]
-				})
-			);
-		}
-
-		holder = element('div', { className: 'test-colorFactor-holder' });
-		holder.append(
-			swatches,
-			element('div', { textContent: label, className: 'test-colorFactory-label' })
-		);
-		document.querySelector(target).append(holder);
-	});
+function eachKeyed(labelKey, dataset) {
+	const result = {};
+	for (let i = 0; i < dataset.length; i++) {
+		result[ dataset[i][labelKey] ] = dataset[i];
+	}
+	return result;
 }
 
-/* Custom tests */
+function eachFmt(labelFmt, dataset) {
+	const result = {};
+	for (let i = 0; i < dataset.length; i++) {
+		const label = labelFmt.replace(/\$(\d+)/g, ( str, num ) => dataset[i][num]);
+		result[ label ] = dataset[i];
+	}
+	return result;
+}
 
-QUnit.colorTest = function (title, actualColors, callback) {
-	return QUnit.test(title, function () {
-		this.renderColors = function (colors, label) {
-			renderColors(colors, '#qunit-test-output-' + QUnit.config.current.testId, label);
-		};
-		this.renderColors(actualColors, 'actual');
+function renderColors(colors, label) {
+	var target = '#qunit-test-output-' + QUnit.config.current.testId;
+	var swatches = element('div', { className: 'test-colorFactory-swatches' });
 
-		this.colors = actualColors;
+	for (var i = 0; i < colors.length; i++) {
+		swatches.append(
+			element('div', {
+				className: 'test-colorFactory-swatch',
+				style: 'background-color:' + colors[colors.length - i - 1]
+			})
+		);
+	}
 
-		return callback.apply(this, arguments);
-	});
-};
-
-QUnit.colorTest.closeMatch = function (title, actualColors, expectedColors, threshold) {
-	return QUnit.colorTest(title, actualColors, function (assert) {
-		this.renderColors(expectedColors, 'match');
-
-		assert.strictEqual(actualColors.length, expectedColors.length, 'validate array size');
-		for (var i = 0, len = expectedColors.length; i < len; i += 1) {
-			assert.colorIsVisuallyClose(actualColors[i], expectedColors[i], threshold);
-		}
-	});
-};
-
-QUnit.colorTest.complementary = function (input, expected) {
-	return QUnit.colorTest.closeMatch('complementaryColors() for ' + input,
-		ColorFactory.complementary(input),
-		[expected, input]
+	var holder = element('div', { className: 'test-colorFactor-holder' });
+	holder.append(
+		swatches,
+		element('div', { textContent: label, className: 'test-colorFactory-label' })
 	);
-};
-
-// The test for binary() is basically the same as for complementary(),
-// both take one value and add one to it. Except that
-// complementary() sets the input last, and binary() sets it first.
-QUnit.colorTest.binary = function (message, input, expected, threshold) {
-	return QUnit.colorTest.closeMatch(message,
-		ColorFactory.binary(input),
-		[input, expected],
-		threshold
-	);
-};
-
-QUnit.colorTest.distinguishable = function (title, colors, callback) {
-	return QUnit.colorTest(title, colors, function (assert) {
-		assert.colorIsDistinguishable(colors);
-		if (callback) {
-			callback.apply(this, arguments);
-		}
-	});
-};
+	document.querySelector(target).append(holder);
+}
 
 /* Custom assertions */
 
@@ -99,20 +55,20 @@ QUnit.assert.match = function (actual, regex, message) {
 };
 
 QUnit.assert.colorIsDistinguishable = function (colors) {
-	var i, c0, c1, hsl0, hsl1, diff;
-
 	if (colors.length < 2) {
 		this.pushFailure('assert.beDistinguishable expects two or more colors.');
 		return;
 	}
 
-	for (i = 0; i < (colors.length - 1); i++) {
-		c0 = colors[i];
-		c1 = colors[i + 1];
-		hsl0 = ColorHelper.rgbToHSL(c0);
-		hsl1 = ColorHelper.rgbToHSL(c1);
+	renderColors(colors, 'actual');
 
-		diff = Math.abs(hsl0[0] - hsl1[0]) + Math.abs(hsl0[1] - hsl1[1]) + Math.abs(hsl0[2] - hsl1[2]);
+	for (let i = 0; i < (colors.length - 1); i++) {
+		let c0 = colors[i];
+		let c1 = colors[i + 1];
+		let hsl0 = ColorHelper.rgbToHSL(c0);
+		let hsl1 = ColorHelper.rgbToHSL(c1);
+
+		let diff = Math.abs(hsl0[0] - hsl1[0]) + Math.abs(hsl0[1] - hsl1[1]) + Math.abs(hsl0[2] - hsl1[2]);
 		this.pushResult( {
 			result: diff > 30,
 			actual: diff,
@@ -125,19 +81,19 @@ QUnit.assert.colorIsDistinguishable = function (colors) {
 	}
 };
 
-QUnit.assert.colorIsVisuallyClose = function (actual, expected, threshold) {
+QUnit.assert.colorCloseTo = function (actual, expected, threshold) {
 	threshold = threshold || 8;
 
-	var d,
-		expectedHsl = ColorHelper.rgbToHSL(expected),
-		actualHsl = ColorHelper.rgbToHSL(actual),
-		diffTotal = 0,
-		diff = [];
-	for (d in actualHsl) {
+	var expectedHsl = ColorHelper.rgbToHSL(expected);
+	var actualHsl = ColorHelper.rgbToHSL(actual);
+
+	var diff = [];
+	for (let d in actualHsl) {
 		diff.push(Math.abs(expectedHsl[d] - actualHsl[d]));
 	}
 
-	for (d in diff) {
+	var diffTotal = 0;
+	for (let d in diff) {
 		diffTotal += diff[d];
 		if (d === 0) {
 			diffTotal /= 3.6;
@@ -150,4 +106,14 @@ QUnit.assert.colorIsVisuallyClose = function (actual, expected, threshold) {
 		expected: diffTotal + ' < (threshold * 3)',
 		message: 'color ' + actual + ' is visually close to ' + expected + ' (within a threshold of: ' + threshold + ')'
 	} );
+};
+
+QUnit.assert.colorArrayCloseTo = function (actualColors, expectedColors, threshold) {
+	renderColors(actualColors, 'actual');
+	renderColors(expectedColors, 'expected');
+	this.strictEqual(actualColors.length, expectedColors.length, 'validate array size');
+
+	for (var i = 0; i < actualColors.length; i++) {
+		this.colorCloseTo(actualColors[i], expectedColors[i], threshold);
+	}
 };
